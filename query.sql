cd67882f00a593383cda408bb74ba765f0c0c9c5
KoreaITAcademy_ACW <50722523+KoreaITAcademy@users.noreply.github.com>
2019-11-24 13:21:51 +0900

    asdfsa
    dfsdf

--- query.sql
+++ query.sql
@@ -8,7 +8,7 @@ CREATE TABLE t_comment(
 );
 
 CREATE TABLE t_board(
-	i_board INT PRIMARY KEY AUTO_INCREMENT,
+	i_board INT unsigned PRIMARY KEY AUTO_INCREMENT,
 	title varCHAR(30) NOT NULL,
 	content TEXT NOT NULL,
 	cnt SMALLINT UNSIGNED DEFAULT '0',
@@ -21,3 +21,16 @@ CREATE TABLE t_user(
 	nm VARCHAR(10) NOT NULL,
 	r_datetime DATETIME DEFAULT NOW()
 );
+
+CREATE TABLE `t_favorite` (
+	`i_board` INT(10) UNSIGNED NOT NULL,
+	`uid` VARCHAR(30) NOT NULL,
+	`r_datetime` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (`i_board`, `uid`),
+	INDEX `uid` (`uid`),
+	CONSTRAINT `t_favorite_ibfk_1` FOREIGN KEY (`i_board`) REFERENCES `t_board` (`i_board`),
+	CONSTRAINT `t_favorite_ibfk_2` FOREIGN KEY (`uid`) REFERENCES `t_user` (`uid`)
+)
+COLLATE='utf8mb4_0900_ai_ci'
+ENGINE=InnoDB
+;
